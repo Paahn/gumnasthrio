e1708d6c6feb308f3c14cc94e219e3fb081869d7
Paahn <paahndev@gmail.com>
2021-04-26 11:50:22 -0700

    Create hoistingExercise.js

---
+++ javascript/hoistingExercise.js
@@ -0,0 +1,16 @@
+// Guess what the console will log, based on hoisting, execution context.
+
+var favouriteFood = "grapes";
+
+var foodThoughts = function () {
+    console.log("Original favourite food: ",
+    favouriteFood);
+
+    var favouriteFood = "sushi";
+
+    console.log("New favourite food: ", favouriteFood);
+};
+
+foodThoughts();
+
+// answer
