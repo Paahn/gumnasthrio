53047acf00dd872caed73aa605b0620b8b22210c
Paahn <paahndev@gmail.com>
2021-04-27 16:26:36 -0700

    solved duplicateZeroes by adding zeroes in place of original input array

--- javascript/duplicateZeroes.js
+++ javascript/duplicateZeroes.js
@@ -44,4 +44,14 @@
 //     return copiedArray;
 // };
 
+const duplicateZeros = function(arr) {
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === 0) {
+            arr.pop();
+            arr.splice(i, 0, 0);
+            i += 1;
+        }
+    }
+};
+
 console.log(duplicateZeros([1,0,2,3,0,4,5,0]));
