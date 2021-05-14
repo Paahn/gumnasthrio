df8e14bc162571006d2449c81d525c8480309c90
Paahn <paahndev@gmail.com>
2021-05-14 12:43:00 -0700

    0(1) solution for removeElement

--- javascript/removeElement.js
+++ javascript/removeElement.js
@@ -50,15 +50,13 @@
  * @return {number}
  */
  var removeElement = function(nums, val) {
-     let i = 0;
-     nums.forEach(num => {
-        if (num === val) {
-           nums.splice(i, 1);
-        }
-        i += 1;
-        // console.log(num);
-     });
+   for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === val) {
+         nums.splice(i, 1);
+         i--;
+      }
+   }
      return nums.length;
 };
 
-console.log(removeElement([0,1,2,2,3,0,4,2]));
+console.log(removeElement([0,1,2,2,3,0,4,2], 2));
