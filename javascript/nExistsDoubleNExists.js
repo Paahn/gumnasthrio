3cbf835f7f71da8300331c5da075ed564df6be29
Paahn <paahndev@gmail.com>
2021-06-07 22:51:03 -0700

    solution for search for double of integer in an array

--- javascript/nExistsDoubleNExists.js
+++ javascript/nExistsDoubleNExists.js
@@ -34,5 +34,14 @@
  * @return {boolean}
  */
  var checkIfExist = function(arr) {
-    
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === 2 * arr[j] || arr[i] * 2 === arr[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
 };
+
+checkIfExist([7,1,14,11]);
